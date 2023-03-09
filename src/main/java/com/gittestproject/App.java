23540cda5eb638de5f12da621dc4a3003b47b425
Youssef Kiouch <kiou.youssef@gmail.com>
2023-03-09 11:19:18 -0500

    initial commit

---
+++ src/main/java/com/gittestproject/App.java
@@ -0,0 +1,13 @@
+package com.gittestproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
